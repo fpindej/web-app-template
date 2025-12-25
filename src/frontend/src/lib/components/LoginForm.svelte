36e48fd4cc491d3d253e512263e8158bdc4670d5
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-12-25 18:43:44 +0100

    Make login screen more responsive

--- src/frontend/src/lib/components/LoginForm.svelte
+++ src/frontend/src/lib/components/LoginForm.svelte
@@ -48,7 +48,7 @@
 	}
 </script>
 
-<div class="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
+<div class="flex min-h-screen flex-col justify-center px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
 	<div class="sm:mx-auto sm:w-full sm:max-w-md">
 		<Card.Root>
 			<Card.Header>
